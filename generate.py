a6e4d1eae4778b1cdce9ca6615c1b4bb3bf5f0ef
Matthew Rice <mhrice@ucsd.edu>
2022-02-18 20:08:02 -0800

    Fix input arguments for generate

--- generate.py
+++ generate.py
@@ -8,10 +8,10 @@ from utils.tokenizer import tokenizer
 def main():
     parser = argparse.ArgumentParser(description='Generate Midi Files')
     
-    parser.add_argument("dataset", help="dataset directory")
-    parser.add_argument("checkpoints", help="Output directory")
+    parser.add_argument("dataset", help="Dataset directory")
+    parser.add_argument("checkpoints", help="Checkpoint path")
     parser.add_argument("out", help="Output midi file name")
-    parser.add_argument("--l", help="Sequence length. Default is 2048", dest="seq_length", default=2048)
+    parser.add_argument("--l", help="Sequence length. Default is 500", dest="seq_length", default=500)
     args = parser.parse_args()
 
     dataset = REMI_dataset(args.dataset)
@@ -27,7 +27,6 @@ def main():
     model.eval()
     with torch.set_grad_enabled(False):
         beam_seq = model.generate(primer[:256], int(args.seq_length), beam=1)
-        print(beam_seq)
     programs = [(0, False), (41, False), (61, False), (0, True)]
     generated_midi = tokenizer.tokens_to_midi(beam_seq.tolist(), programs)
     generated_midi.dump(args.out)         
